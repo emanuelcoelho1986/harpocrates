73b36962f52e23009549f5478bae5e6265b51e80
Emanuel Coelho <emanuelcoelho1986@gmail.com>
2022-04-15 22:11:01 +0100

    simplify[react] launch tests and scripts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,9 @@
-import { render, screen } from '@testing-library/react'
-
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-describe('<App />', () => {
-    beforeEach(() => {
-        render(<App />);
-    });
-
-    it('should render App with default values', () => {
-        expect(screen.getByText(/Hello Vite \+ React!/i)).toBeInTheDocument();
-    })
-})
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
